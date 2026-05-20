94ed4143693ca496580abe10be190831fd403645
mr.ahmedalathwari@gmail.com <mr.ahmedalathwari2@gmail.com>
2026-05-20 22:34:38 +0300

    Create core database structure for store modules

---
+++ database/migrations/2026_05_20_192934_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    
+   public function up(): void
+{
+    Schema::create('comments', function (Blueprint $table) {
+
+        $table->id();
+
+        $table->foreignId('product_id');
+
+        $table->foreignId('user_id');
+
+        $table->text('comment');
+
+        $table->integer('rate');
+
+        $table->string('ip')->nullable();
+
+        $table->boolean('status')->default(true);
+
+        $table->timestamps();
+
+    });
+}
+
+   
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
