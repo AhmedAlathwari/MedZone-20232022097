fc8af6192ec6d7d9d64b8d0ddec45179d56dfaa7
mr.ahmedalathwari@gmail.com <mr.ahmedalathwari2@gmail.com>
2026-05-20 22:55:20 +0300

    Add store database tables and order structure

---
+++ database/migrations/2026_05_20_194133_create_orders_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  
+    public function up(): void
+{
+    Schema::create('orders', function (Blueprint $table) {
+
+        $table->id();
+
+        $table->foreignId('user_id');
+
+        $table->string('name');
+
+        $table->string('surname');
+
+        $table->string('email');
+
+        $table->string('phone');
+
+        $table->string('address');
+
+        $table->double('total');
+
+        $table->string('ip')->nullable();
+
+        $table->string('status')->default('new');
+
+        $table->timestamps();
+
+    });
+}
+
+    
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
