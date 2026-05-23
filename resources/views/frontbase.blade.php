85f19b0bf2974853901ea7e3ffff39b947c2505a
mr.ahmedalathwari@gmail.com <mr.ahmedalathwari2@gmail.com>
2026-05-23 03:39:56 +0300

    Display site favicon on frontend pages

--- resources/views/frontbase.blade.php
+++ resources/views/frontbase.blade.php
@@ -4,9 +4,25 @@
 
 <head>
 
+@php
+
+$setting =
+\App\Models\Setting::first();
+
+@endphp
+
 <title>
 @yield('title')
 </title>
+
+@if($setting && $setting->icon)
+
+<link
+rel="icon"
+type="image/png"
+href="{{ Storage::url($setting->icon) }}">
+
+@endif
 
 <link rel="stylesheet"
 href="{{ asset('assets/css/style.css') }}">
