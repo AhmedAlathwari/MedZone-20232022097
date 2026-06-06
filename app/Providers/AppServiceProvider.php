8d9638313862d5968eb33e478387ec086e69ea9e
mr.ahmedalathwari@gmail.com <mr.ahmedalathwari2@gmail.com>
2026-06-06 06:54:51 +0300

    Fix AppServiceProvider syntax

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,9 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-{
-    if (env('APP_ENV') === 'production') {
-        URL::forceScheme('https');
+    {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
