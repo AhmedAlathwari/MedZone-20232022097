73842efc3e56d9f779a17fb263d479457ccb3882
mr.ahmedalathwari@gmail.com <mr.ahmedalathwari2@gmail.com>
2026-06-07 00:29:24 +0300

    Create admin role and assign admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Role;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -10,12 +11,20 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::updateOrCreate(
+        $adminRole = Role::firstOrCreate([
+            'name' => 'admin',
+        ]);
+
+        $adminUser = User::updateOrCreate(
             ['email' => 'admin@medzone.com'],
             [
                 'name' => 'MedZone Admin',
                 'password' => Hash::make('password'),
             ]
         );
+
+        $adminUser->roleList()->syncWithoutDetaching([
+            $adminRole->id
+        ]);
     }
 }
